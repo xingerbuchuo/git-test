c955bdb5baafb294a3038bcdd0727749e025becc
zengjiahui <13657073433@163.com>
2023-02-20 16:21:28 +0800

    master first commit

---
+++ src/main/java/com/zjh/gittest/GitTestDemo1.java
@@ -0,0 +1,15 @@
+package com.zjh.gittest;
+
+/**
+ * @BelongsProject: git-test
+ * @BelongsPackage: com.zjh.gittest
+ * @Author: Zeng JiaHui
+ * @CreateTime: 2023/2/20 16:20
+ * @Description:
+ * @Version: 1.0
+ */
+public class GitTestDemo1 {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
